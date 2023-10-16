b6aaca895bae371b48df41755bc3db5d4d2de7e9
nknikhil2013 <nknikhi2013@gmail.com>
2023-10-16 23:53:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Screen from './components/Appscreen';
+
+function App() {
+  return (
+    <div className='bg-dark container-fluid'>
+      <Screen />
+    </div>
+  );
+}
+
+export default App;
